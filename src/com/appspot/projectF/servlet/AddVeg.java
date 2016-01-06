9f8aa2446f8f06e522434d47950ae8f2d5bcb5b4
t13cs040 <t13cs040@gmail.com>
2016-01-06 10:56:22 +0900

    Update AddVeg.java

--- src/com/appspot/projectF/servlet/AddVeg.java
+++ src/com/appspot/projectF/servlet/AddVeg.java
@@ -1,9 +1,8 @@
-package servlet;
+package com.appspot.projectF.servlet;
 
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AddVeg
  */
-@WebServlet("/AddVeg")
 public class AddVeg extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
