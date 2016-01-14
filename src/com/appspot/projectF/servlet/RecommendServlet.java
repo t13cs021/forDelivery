8d1c24e343832e912365c4aa0d6232c79e5408bf
t13cs040 <t13cs040@yamanashi.ac.jp>
2016-01-14 14:24:19 +0900

    recommendの細かい修正
    jspの文字コード対応（もう県名が文字化けしないよ！）
    星によるランク分けに対応
    変数名などわかりやすく修正

--- src/com/appspot/projectF/servlet/RecommendServlet.java
+++ src/com/appspot/projectF/servlet/RecommendServlet.java
@@ -11,20 +11,33 @@ public class RecommendServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// いっかいきえたかなしみにまけててきとう
-		String str = req.getParameter("pref");
+		/***** 変数宣言 *****/
+		//前のページで選択された都道府県名を取得
+		String pref = req.getParameter("pref");
+		//選出された野菜
+		String[] rank1_crops = {"hoge", "fuga"};
+		String[] rank2_crops = {"foo", "bar"};
 		
+		/***** データ取得 *****/
+		
+		
+		
+		
+		
+		/***** 選出 *****/
 		/*
 		 * このへんでDBとかアクセスしていろいろ算出してsetAttributeしてあげる
 		 * ほかのServletも同じ流れ
 		 */
 		
-		req.setAttribute("prefName", str);
+		/***** 以下，jspに渡すデータ *****/
+		//選択された都道府県名
+		req.setAttribute("prefName", pref);
+		// 選出された野菜一覧
+		req.setAttribute("rank1_crops", rank1_crops);
+		req.setAttribute("rank2_crops", rank2_crops);
 		
-		// sample
-		String[] mozis = {"hoge", "fuga", "foo", "bar"};
-		req.setAttribute("mozis", mozis);
-		
+		/***** ページ遷移 *****/
 		req.getRequestDispatcher("/recommend.jsp").forward(req, resp);
 	}
 }
