a81fad8614951a47be937406444b0d87922bd055
t13cs064 <t13cs064@gmail.com>
2016-01-15 14:57:20 +0900

    count star and show in recommend.jsp

--- src/com/appspot/projectF/servlet/RecommendServlet.java
+++ src/com/appspot/projectF/servlet/RecommendServlet.java
@@ -22,7 +22,8 @@ public class RecommendServlet extends HttpServlet {
 		String pref = req.getParameter("pref");
 		//選出された野菜
 		List<String> rank1_crops = new ArrayList<String>();
-		String[] rank2_crops = {"foo", "bar"};
+		List<String> rank2_crops = new ArrayList<String>();
+		String[] debug_crops = {"foo", "bar"};
 		//取得した農作物データ
 		List<Crops> crops;
 		//取得した気象予測データ
@@ -46,17 +47,30 @@ public class RecommendServlet extends HttpServlet {
 		climates=forecast.getForecast(pref);
 		//デバッグ用表示
 		for(int i=1;i<13;i++){
-			System.out.println(climates[i].getPrefectures() + " " + climates[i].getYear() + " " + climates[i].getMonth());
+			System.out.println(climates[i].getPrefectures() + "," + climates[i].getYear() + "," + climates[i].getMonth() + "::" + climates[i].getTemperature() + "," + climates[i].getSunhour());
 		}
 		
 		/***** 選出 *****/
-		for(Crops crop:crops){
-			//作物情報を作物名ごとに分類
-			//すべての農作物について適合するか確認
-			//rank1_crops,rank2_cropsに格納
-			
-			//一旦テストとして全ての農作物を選出
-			rank1_crops.add(crop.getName());
+		Map<String, List<Crops>> cropMap = getMapWithName(crops);
+		for (List<Crops> l: cropMap.values()) {
+			int items = l.size();
+			if (items == 0) return;
+			String vegName = l.get(0).getName();
+			int tempcount = 0, suncount = 0;
+			for (Crops c: l) {
+				int month = c.getMonth();
+				float forecastTemp = climates[month].getTemperature();
+				float forecastSun  = climates[month].getSunhour();
+				if (c.getTemp_lLimit() <= forecastTemp && forecastTemp <= c.getTemp_uLimit()) tempcount++;
+				if (c.getSunhour_lLimit() <= forecastSun && forecastSun <= c.getSunhour_uLimit()) suncount++;
+			}
+			int result = (tempcount == items ? 1 : 0) + (suncount == items ? 1 : 0);
+			System.out.println(vegName + "'s result is " + result + "(" + tempcount + "," + suncount + ")");
+			if (result == 2) {
+				rank2_crops.add(vegName);
+			} else if (result == 1) {
+				rank1_crops.add(vegName);
+			}
 		}
 		
 		
@@ -66,8 +80,25 @@ public class RecommendServlet extends HttpServlet {
 		// 選出された野菜一覧
 		req.setAttribute("rank1_crops", rank1_crops);
 		req.setAttribute("rank2_crops", rank2_crops);
+		req.setAttribute("debug_crops", debug_crops);
 		
 		/***** ページ遷移 *****/
 		req.getRequestDispatcher("/recommend.jsp").forward(req, resp);
+	}
+
+	private static Map<String, List<Crops>> getMapWithName(List<Crops> allCrops) {
+		Set<String> nameSet = new HashSet<String>();
+		
+		for (Crops c: allCrops) nameSet.add(c.getName());
+		
+		Map<String, List<Crops>> map = new HashMap<String, List<Crops>>();
+		
+		for (String vegName: nameSet) map.put(vegName, new LinkedList<Crops>());
+		
+		for (Crops c: allCrops) {
+			map.get(c.getName()).add(c);
+		}
+		
+		return map;
 	}
 }
