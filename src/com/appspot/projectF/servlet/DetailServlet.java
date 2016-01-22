56bb7e17688ee2c98a6b484f43954c25bebe5e27
t13cs064 <t13cs064@gmail.com>
2016-01-22 13:58:00 +0900

    divide detail.jsp (refactoring)

--- src/com/appspot/projectF/servlet/DetailServlet.java
+++ src/com/appspot/projectF/servlet/DetailServlet.java
@@ -1,11 +1,17 @@
 package com.appspot.projectF.servlet;
 
 import java.io.IOException;
+import java.util.*;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.appspot.projectF.datastore.*;
+import com.appspot.projectF.util.*;
 
 public class DetailServlet extends HttpServlet {
 	@Override
@@ -14,6 +20,27 @@ public class DetailServlet extends HttpServlet {
 		// いっかいきえたかなしみにまけててきとう
 		String str = req.getParameter("pref");
 		String veg = req.getParameter("yasai");
+		
+		
+		PersistenceManager pm = null;
+		//DB開けなかったら，開けてもデータストアを閉じる
+		try {
+			pm = PMF.get().getPersistenceManager();
+			Query query = pm.newQuery(Crops.class);
+
+			List<Crops> allcrops = (List<Crops>) query.execute();
+			List<Crops> crops = new ArrayList<Crops>();
+			for (Crops sr : allcrops) {
+				if (veg.equals(sr.getName())) {
+					crops.add(sr);
+				}
+			}
+			Sort.sortCropsForMonth(crops);
+			req.setAttribute("crops", crops);
+		} finally {
+			if (pm != null && !pm.isClosed())
+				pm.close();
+		}
 		
 		req.setAttribute("prefName", str);
 		req.setAttribute("vegName", veg);
